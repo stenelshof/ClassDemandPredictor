b77e2bf922b9e7cabd16fda256480104f50c042c
Silas Ten Elshof <stenelshof@westmont.edu>
2024-01-31 00:21:11 -0800

    getting project setup on PyCharm

---
+++ src/ClassPredictor/ClassDemandPredictor.py
@@ -0,0 +1,5 @@
+__author__ = "Silas Ten Elshof"
+__copyright__ = "Copyright 2024, Westmont College"
+__credits__ = ["Silas Ten Elshof", "Mike Ryu"]
+__license__ = "MIT"
+__email__ = "Stenelshof@westmont.edu"
